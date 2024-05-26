04c62282e902401dced8dc09b5c1196723ab7fde
filiperak <filip.erak@googlemail.com>
2024-05-26 17:21:26 +0200

    test server

--- server/index.js
+++ server/index.js
@@ -9,13 +9,20 @@ app.use(express.json())
 
 mongoose.connect(mongoDBURL)
 
-app.post('add',(req,res) => {
+app.post('/add',(req,res) => {
     const item = req.body.item;
     ItemModel.create({
         item: item
     }).then(result => res.json(result))
     .catch( e => console.log(e))
 })
+
+app.get('/items', (req, res) => {
+    ItemModel.find()
+    .then(items => res.json(items))
+    .then(items => console.log(items))
+    .catch(e => res.status(500).json({ error: e.message }));
+});
 
 app.listen(5000, () => {
     console.log(('server is running'));
