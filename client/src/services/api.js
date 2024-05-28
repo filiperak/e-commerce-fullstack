f3e97605ecce4b7f1291d1355a72ba21cce478be
filiperak <filip.erak@googlemail.com>
2024-05-28 14:42:28 +0200

    build for ec2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,3 +1,3 @@
-export const api = `http://34.197.36.155:5000`;
+export const api = `http://44.198.244.147:5000`;
 // export const api = `http://localhost:5000`;
 // export const api = `https://dummyjson.com/products`;
