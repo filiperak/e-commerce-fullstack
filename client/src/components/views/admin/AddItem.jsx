6f41154e2b9b1dc40df142fd7f656e6a286ccc84
filiperak <filip.erak@googlemail.com>
2024-06-02 01:14:17 +0200

    additem

--- client/src/components/views/admin/AddItem.jsx
+++ client/src/components/views/admin/AddItem.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import { AdminContainer, EditContainerTitle, EditDescription, EditInputField, EditProductDetails, EdititemContainer, EdititemImages, EdititemProductData } from './styled';
-import { api } from '../../../services/api';
-import ProductNav from '../../products/ProductNav';
-import AdminEditHeader from '../../adminComponents/AdminEditHeader';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  AdminContainer,
+  EditContainerTitle,
+  EditDescription,
+  EditInputField,
+  EditProductDetails,
+  EdititemContainer,
+  EdititemImages,
+  EdititemProductData,
+} from "./styled";
+import { api } from "../../../services/api";
+import ProductNav from "../../products/ProductNav";
+import AdminEditHeader from "../../adminComponents/AdminEditHeader";
+import { useNavigate } from "react-router-dom";
 
 const AddItem = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [newItem, setNewItem] = useState({
     title: "",
     category: "",
@@ -20,27 +29,27 @@ const AddItem = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setNewItem(prevState => ({
+    setNewItem((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
+    //e.preventDefault();
     console.log(newItem);
 
     try {
       const response = await fetch(`${api}/items/`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(newItem),
       });
 
       if (!response.ok) {
-        throw new Error('failed');
+        throw new Error("failed");
       }
 
       setNewItem({
@@ -54,21 +63,25 @@ const AddItem = () => {
         brand: "",
       });
 
-      console.log('item added');
+      console.log("item added");
     } catch (error) {
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
     }
   };
   const handleCancle = () => {
-    navigate('/admin')
-  }
+    navigate("/admin");
+  };
 
   return (
-  <>
-  <ProductNav prev={'admin'}/>
-  <AdminEditHeader 
- onCancel={handleCancle} text={'Create New Product'} btnTxt={'Add Product'}/>
-  <EdititemContainer>
+    <>
+      <ProductNav prev={"admin"} />
+      <AdminEditHeader
+        onCancel={handleCancle}
+        onSave={handleSubmit}
+        text={"Create New Product"}
+        btnTxt={"Add Product"}
+      />
+      <EdititemContainer>
         <EdititemImages>
           <EditContainerTitle>Product Images</EditContainerTitle>
           123
@@ -78,114 +91,141 @@ const AddItem = () => {
           <EditProductDetails>
             <EditInputField>
               <label htmlFor="title">Product title</label>
-              <input type="text" name="title" id="title" />
+              <input
+                type="text"
+                name="title"
+                id="title"
+                onChange={handleChange}
+              />
             </EditInputField>
 
             <EditInputField>
               <label htmlFor="brand">Product Brand</label>
-              <input type="text" name="brand" id="brand"  />
+              <input
+                type="text"
+                name="brand"
+                id="brand"
+                onChange={handleChange}
+              />
             </EditInputField>
             <EditInputField>
               <label htmlFor="price">Product price</label>
-              <input type="text" name="price" id="price"  />
+              <input
+                type="text"
+                name="price"
+                id="price"
+                onChange={handleChange}
+              />
             </EditInputField>
             <EditInputField>
               <label htmlFor="rating">Product rating</label>
-              <input type="text" name="rating" id="rating" />
+              <input
+                type="text"
+                name="rating"
+                id="rating"
+                onChange={handleChange}
+              />
             </EditInputField>
             <EditInputField>
               <label htmlFor="category">Product category</label>
-              <input type="text" name="category" id="category" />
+              <input
+                type="text"
+                name="category"
+                id="category"
+                onChange={handleChange}
+              />
             </EditInputField>
           </EditProductDetails>
           <EditDescription>
             <p>Product Description</p>
-            <textarea name="description" id="description"></textarea>
+            <textarea
+              name="description"
+              id="description"
+              onChange={handleChange}
+            ></textarea>
           </EditDescription>
         </EdititemProductData>
       </EdititemContainer>
+{/* 
+      <AdminContainer onSubmit={handleSubmit} style={{ marginTop: "100px" }}>
+        <h3>Create new item</h3>
+        <p>Enter item title</p>
+        <input
+          type="text"
+          value={newItem.title}
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item category</p>
+        <input
+          type="text"
+          value={newItem.category}
+          name="category"
+          placeholder="Category"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item price</p>
+        <input
+          type="number"
+          value={newItem.price}
+          name="price"
+          placeholder="Price"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item thumbnail image</p>
 
- <AdminContainer onSubmit={handleSubmit} style={{ marginTop: '100px' }}>
-      <h3>Create new item</h3>
-            <p>Enter item title</p>
-      <input
-        type="text"
-        value={newItem.title}
-        name="title"
-        placeholder="Title"
-        onChange={handleChange}
-        required
-      />
-            <p>Enter item category</p>
-      <input
-        type="text"
-        value={newItem.category}
-        name="category"
-        placeholder="Category"
-        onChange={handleChange}
-        required
-      />
-          <p>Enter item price</p>
-      <input
-        type="number"
-        value={newItem.price}
-        name="price"
-        placeholder="Price"
-        onChange={handleChange}
-        required
-      />
-            <p>Enter item thumbnail image</p>
-
-      <input
-        type="text"
-        value={newItem.thumbnail}
-        name="thumbnail"
-        placeholder="Thumbnail"
-        onChange={handleChange}
-        required
-      />
-                  <p>Enter item rating</p>
-      <input
-        type="number"
-        value={newItem.rating}
-        name="rating"
-        placeholder="Rating"
-        onChange={handleChange}
-        required
-      />
-                  <p>Enter item image</p>
-      <input
-        type="text"
-        value={newItem.images}
-        name="images"
-        placeholder="Images"
-        onChange={handleChange}
-        required
-      />
-                  <p>Enter item description</p>
-      <input
-        type="text"
-        value={newItem.description}
-        name="description"
-        placeholder="Description"
-        onChange={handleChange}
-        required
-      />
-                  <p>Enter item brand</p>
-      <input
-        type="text"
-        value={newItem.brand}
-        name="brand"
-        placeholder="Brand"
-        onChange={handleChange}
-        required
-      />
-      <input type="submit" value="ADD NEW ITEM" id='add-item-btn' />
-    </AdminContainer>
-  </>
-   
+        <input
+          type="text"
+          value={newItem.thumbnail}
+          name="thumbnail"
+          placeholder="Thumbnail"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item rating</p>
+        <input
+          type="number"
+          value={newItem.rating}
+          name="rating"
+          placeholder="Rating"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item image</p>
+        <input
+          type="text"
+          value={newItem.images}
+          name="images"
+          placeholder="Images"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item description</p>
+        <input
+          type="text"
+          value={newItem.description}
+          name="description"
+          placeholder="Description"
+          onChange={handleChange}
+          required
+        />
+        <p>Enter item brand</p>
+        <input
+          type="text"
+          value={newItem.brand}
+          name="brand"
+          placeholder="Brand"
+          onChange={handleChange}
+          required
+        />
+        <input type="submit" value="ADD NEW ITEM" id="add-item-btn" />
+      </AdminContainer> */}
+    </>
   );
 };
 
 export default AddItem;
-
