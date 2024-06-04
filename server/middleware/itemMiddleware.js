ab68a341260a449ce6a805516446576cdebe255c
filiperak <filip.erak@googlemail.com>
2024-06-04 15:14:08 +0200

    middleware

--- server/middleware/itemMiddleware.js
+++ server/middleware/itemMiddleware.js
@@ -1,15 +1,15 @@
 const Item = require('../models/item');
 
 const getItems = async (req, res, next) => {
-    let item;
+    let item
     try {
         item = await Item.findById(req.params.id);
-        if (item === null) return res.status(404).json({ message: "Can't find item" });
+        if (item === null) return res.status(404).json({ message: "cant find item" });
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
     res.item = item;
-    next();
+    next()
 };
 
 module.exports = getItems;
