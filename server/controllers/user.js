852af512e93a1697de49704ed650f9f565f02f0a
filiperak <filip.erak@googlemail.com>
2024-06-10 20:35:13 +0200

    user rroute

---
+++ server/controllers/user.js
@@ -0,0 +1,14 @@
+const User = require('../models/user')
+
+const getAllUsers = async(req,res) => {
+    try {
+        const user = await User.find();
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+module.exports = {
+    getAllUsers
+}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 app.use(express.json());
 app.use(cors())
 
+const PORT = process.env.PORT || 5000
+
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
@@ -15,4 +17,7 @@ db.once('open', () => console.log('Connected to Database'));
 const itemsRouter = require('./routes/item');
 app.use('/items', itemsRouter);
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+const userRouter = require('./routes/user')
+app.use('/user',userRouter)
+
+app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
