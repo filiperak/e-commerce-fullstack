668fc989f9d0b7ca4a9086524f1a80b4aaaecf2c
filiperak <filip.erak@googlemail.com>
2024-06-10 20:44:43 +0200

    create user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,14 +1,28 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
-const getAllUsers = async(req,res) => {
-    try {
-        const user = await User.find();
-        res.json(user)
-    } catch (error) {
-        res.status(500).json({message:error.message})
-    }
-}
+const getAllUsers = async (req, res) => {
+  try {
+    const user = await User.find();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  const user = newUser({
+    username: req.body.username,
+    email: req.body.username,
+    password: req.body.password,
+  });
+  try {
+    const newUser = await user.save()
+    res.status(200).json(newUser)
+  } catch (error) {
+
+  }
+};
 
 module.exports = {
-    getAllUsers
-}
+  getAllUsers,
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getAllUsers} = require('../controllers/user')
+const {getAllUsers,createUser} = require('../controllers/user')
 
 router.get('/',getAllUsers);
+
+router.post('/',createUser)
 
 module.exports = router;
